90bfe2e541bb8f6cb23a0439828125b04ef66e91
outfrost <kotlet.bahn@gmail.com>
2017-11-11 04:20:36 +0100

    Add ArrayList constructors to JobOrder

--- src/main/java/outfrost/algorithmdesign/JobOrder.java
+++ src/main/java/outfrost/algorithmdesign/JobOrder.java
@@ -1,9 +1,22 @@
 package outfrost.algorithmdesign;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class JobOrder extends ArrayList<Job> {
-
+	
+	public JobOrder() {
+		super();
+	}
+	
+	public JobOrder(Collection<? extends Job> c) {
+		super(c);
+	}
+	
+	public JobOrder(int initialCapacity) {
+		super(initialCapacity);
+	}
+	
 	public int totalWeightedTardiness() {
 		int result = 0;
 		int moment = 0;
