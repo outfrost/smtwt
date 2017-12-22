6fb143ff7258d0003df2a7b4205263c812dae71a
outfrost <kotlet.bahn@gmail.com>
2017-12-22 19:54:55 +0100

    Remove unused imports

--- src/main/java/outfrost/algorithmdesign/JobOrderLoader.java
+++ src/main/java/outfrost/algorithmdesign/JobOrderLoader.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.function.Predicate;
 
 public class JobOrderLoader {
 	
