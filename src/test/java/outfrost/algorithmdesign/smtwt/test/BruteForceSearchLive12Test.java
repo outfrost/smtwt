68977b97ffe3af224b3eb1a47cffcd6b76443d71
outfrost <kotlet.bahn@gmail.com>
2018-10-18 23:34:22 +0200

    That was waaay too long

--- src/test/java/outfrost/algorithmdesign/smtwt/test/BruteForceSearchLive12Test.java
+++ src/test/java/outfrost/algorithmdesign/smtwt/test/BruteForceSearchLive12Test.java
@@ -8,31 +8,29 @@ import outfrost.algorithmdesign.smtwt.util.SmallwstLoader;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class BruteForceSearchLive14Test extends Test {
+public class BruteForceSearchLive12Test extends Test {
 	
 	@Override
 	public void run() {
 		System.out.println("Running live data test with brute force search...");
-		System.out.println("14-job smallWST instance");
+		System.out.println("12-job smallWST instance");
 		
 		try {
-			JobOrder jobs = SmallwstLoader.load("data/smallwst/data14.txt");
+			JobOrder jobs = SmallwstLoader.load("data/smallwst/data12.txt");
 			
 			JobOrder expectedOrder = new JobOrder(Arrays.asList(
 				jobs.get(5),
 				jobs.get(0),
 				jobs.get(1),
-				jobs.get(3),
 				jobs.get(11),
 				jobs.get(4),
-				jobs.get(7),
-				jobs.get(8),
 				jobs.get(2),
-				jobs.get(10),
-				jobs.get(13),
+				jobs.get(3),
+				jobs.get(8),
 				jobs.get(9),
+				jobs.get(10),
 				jobs.get(6),
-				jobs.get(12)
+				jobs.get(7)
 			));
 			
 			System.out.println("Initial sequence:");
@@ -51,7 +49,7 @@ public class BruteForceSearchLive14Test extends Test {
 			System.out.println("Time taken: " + timeTaken + " ns");
 			
 			if (!jobs.equals(expectedOrder)) {
-				System.err.println("BruteForceSearchLive14Test: The result did not match the expected sequence");
+				System.err.println("BruteForceSearchLive12Test: The result did not match the expected sequence");
 			}
 		} catch (IOException e) {
 			System.err.println("Error loading problem instance: " + e.getMessage());
